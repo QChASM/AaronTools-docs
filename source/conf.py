7b3dff6f8600308e05a4f3ec6b41d4b743a17ab1
ajs99778 <ajs99778@uga.edu>
2023-06-25 16:07:37 -0600

    testing robots.txt and sitemap

--- source/conf.py
+++ source/conf.py
@@ -38,6 +38,8 @@ extensions = [
     "sphinx_sitemap",
 ]
 
+html_baseurl = "https://aarontools.readthedocs.io/en/latest/"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
